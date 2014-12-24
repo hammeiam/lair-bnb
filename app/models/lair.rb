86bc629e13a69f46e94302b6d5f0a38c7f0ebff1
David Hamme <dhamme@gmail.com>
2014-12-24 02:07:57 -0800

    removed presence validation for country from lair model since it's not required

--- app/models/lair.rb
+++ app/models/lair.rb
@@ -28,7 +28,7 @@ class Lair < ActiveRecord::Base
 	accepts_nested_attributes_for :images
 
 	validates :title, :description, :rate, :owner_id, :lair_type, 
-		:room_type, :street_address, :city, :state, :country, presence: true
+		:room_type, :street_address, :city, :state, presence: true
 	geocoded_by :full_street_address   # can also be an IP address
 	after_validation :geocode          # auto-fetch coordinates
 	self.per_page = 4
